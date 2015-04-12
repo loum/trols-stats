8b632ad6337d5eecbc7cf23412af642328483bd1
Lou Markovski <lou.markovski@gmail.com>
2015-04-12 21:11:11 +1000

    task/ta-190-integrate-scraper-into-loader-interface
    trols_stats/interface/loader.py
    * new method load() that scrapes and generates
      trols_stats.model.aggregate.Game objects

--- trols_stats/interface/loader.py
+++ trols_stats/interface/loader.py
@@ -1,4 +1,6 @@
 import trols_stats
+import trols_stats.interface
+from logga.log import log
 
 __all__ = ['Loader']
 
@@ -18,3 +20,46 @@ class Loader(object):
 
     def __init__(self):
         self.__games = []
+
+    def load(self, html):
+        """Scrape and load *html* page.
+
+        Produces a list of :class:`trols_stats.model.aggregates.Games`
+        objects that are appended to the :attr:`games` attribute.
+
+        **Args:**
+            *html*: string representation of the HTML page to process.
+            *html* is typically a TROLS match results page.
+
+        """
+        # Scrape match teams.
+        xpath = '//table/tr/td/b'
+        color_xpath = "//table/tr/td/b[contains(text(), '%s')]/span/text()"
+        teams = trols_stats.Scraper.scrape_match_teams(html,
+                                                       xpath,
+                                                       color_xpath)
+
+        # Scrape player names.
+        players = trols_stats.Scraper.scrape_player_names(html)
+
+        # Scrape match preamble.
+        xpath = '//table/tr/td[contains(@class, "mb")]/text()'
+        preamble = trols_stats.Scraper.scrape_match_preamble(html, xpath)
+
+        # Scrape match scores.
+        xpath = '//td/table/tr[contains(@valign, "top")]/td'
+        match_data = trols_stats.Scraper.scrape_match_scores(html, xpath)
+
+        # Augment the data structures.
+        #
+        # Fixture needs the teams.
+        fixture = preamble.copy()
+        fixture.update(teams)
+        log.debug('Fixture: %s' % fixture)
+
+        # Build the Game aggregate object.
+        stats = trols_stats.Stats(players=dict(players),
+                                  teams=teams,
+                                  fixture=fixture)
+        stats.build_game_aggregate(match_data)
+        self.games.extend(stats.games_cache)
--- trols_stats/interface/tests/test_loader.py
+++ trols_stats/interface/tests/test_loader.py
@@ -1,9 +1,22 @@
 import unittest2
+import os
 
 import trols_stats.interface
 
 
 class TestLoader(unittest2.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        test_files_dir = os.path.join('trols_stats',
+                                      'tests',
+                                      'files',
+                                      'www.trols.org.au',
+                                      'nejta')
+        detailed_results_page = 'match_popup.php?matchid=AA039054.html'
+
+        html_fh = open(os.path.join(test_files_dir, detailed_results_page))
+        cls._detailed_results_html = html_fh.read()
+        html_fh.close()
 
     def test_init(self):
         """Initialise a trols_stats.interface.Loader object.
@@ -11,3 +24,23 @@ class TestLoader(unittest2.TestCase):
         loader = trols_stats.interface.Loader()
         msg = 'Object is not a trols_stats.interface.Loader'
         self.assertIsInstance(loader, trols_stats.interface.Loader, msg)
+
+    def test_load(self):
+        """Scrape and load.
+        """
+        # Given a TROLS detailed match results page
+        html = self._detailed_results_html
+
+        # when a scrape and load occurs
+        loader = trols_stats.interface.Loader()
+        loader.load(html)
+        received = loader.games
+
+        # then I should receive a list of Game objects
+        expected = 16
+        msg = 'trols_stats.interface.Loader.games list length should be 8'
+        self.assertEqual(len(received), expected, msg)
+
+    @classmethod
+    def teatDownClass(cls):
+        cls._detailed_results_html = None
