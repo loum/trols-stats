ef710a88d8d8144daab02d41cfbf872f33d25e82
Lou Markovski <lou.markovski@gmail.com>
2015-07-01 18:47:28 +1000

    task/238-persist-game-aggregates
    trols_stats/bin/trols-stats
    * scrape() function now persists the trols_stats.models.Games
      aggregate
    
    trols_stats/interface/reporter.py
    * __init__() is DB aware

--- trols_stats/interface/reporter.py
+++ trols_stats/interface/reporter.py
@@ -1,6 +1,16 @@
+import trols_stats
 from logga.log import log
 
 
 class Reporter(object):
-    def __init__(self):
-        log.debug('Inside the interface.Reporter')
+    @property
+    def db(self):
+        return self.__db
+
+    @db.setter
+    def db(self, value):
+        self.__db = value
+
+    def __init__(self, shelve=None):
+        self.__db = trols_stats.DBSession(shelve=shelve)
+        self.__db.connect()
