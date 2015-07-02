d97f00beaae030af7e2dc752119716eec8fd50b7
Lou Markovski <lou.markovski@gmail.com>
2015-07-02 18:32:55 +1000

    feature/52-player-lookup
    trols_stats/interface/reporter.py
    * new method get_players() that filters and uniques all
      players|section|team in TROLS

--- trols_stats/interface/reporter.py
+++ trols_stats/interface/reporter.py
@@ -14,3 +14,44 @@ class Reporter(object):
     def __init__(self, shelve=None):
         self.__db = trols_stats.DBSession(shelve=shelve)
         self.__db.connect()
+
+    def get_players(self, name=None):
+        """Get all players from cache.
+
+        **Args:**
+            *name*: name to filter DB against
+
+        **Returns:**
+            list of simplified player dictionaries if the form::
+
+            {
+                'name': 'Isabella Markovski',
+                'section': 14,
+                'team': u'Watsonia Red'
+            }
+
+        """
+        log.debug('Name: %s', name)
+
+        def player_team(match):
+            return {
+                'name': match.get('player').get('name'),
+                'team': match.get('player').get('team'),
+                'section': match.get('fixture').get('section')
+            }
+
+        def player_match(match, name=name):
+            return match.get('player').get('name') == name
+
+        db = self.db.connection['trols']
+
+        matched = []
+        if name is not None:
+            matched = [player_team(x) for x in db if player_match(x)]
+        else:
+            matched = [player_team(x) for x in db]
+
+        # Uniquefy the dictionaries.
+        players = [dict(t) for t in set([tuple(d.items()) for d in matched])]
+
+        return players
--- trols_stats/interface/tests/test_reporter.py
+++ trols_stats/interface/tests/test_reporter.py
@@ -1,12 +1,62 @@
 import unittest2
+import os
 
 import trols_stats.interface as interface
 
 
 class TestReporter(unittest2.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        shelve_dir = os.path.join('trols_stats',
+                                  'interface',
+                                  'tests',
+                                  'files')
+
+        cls.__reporter = interface.Reporter(shelve=shelve_dir)
+
     def test_init(self):
         """Initialise an interface.Reporter object
         """
-        reporter = interface.Reporter()
         msg = 'Object is not a interface.Reporter'
-        self.assertIsInstance(reporter, interface.Reporter, msg)
+        self.assertIsInstance(self.__reporter, interface.Reporter, msg)
+
+    def test_player_cache_player_match(self):
+        """Player cache: player match.
+        """
+        # Given a Games store
+
+        # when I query a player
+        received = self.__reporter.get_players('Isabella Markovski')
+
+        # then I should get the player profile
+        expected = [
+            {
+                'name': 'Isabella Markovski',
+                'section': 14,
+                'team': u'Watsonia Red'
+            },
+            {
+                'name': 'Isabella Markovski',
+                'section': 14,
+                'team': u'Watsonia Blue'
+            }
+        ]
+        msg = 'Player cache mismatch'
+        self.assertListEqual(received, expected, msg)
+
+    def test_player_cache_match_all_players(self):
+        """Player cache: all players.
+        """
+        # Given a Games store
+
+        # when I query a player
+        received = len(self.__reporter.get_players())
+
+        # then I should get the player profile
+        expected = 3729
+        msg = 'Player cache incorrect count'
+        self.assertEqual(received, expected, msg)
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.__reporter
